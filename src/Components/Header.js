8f114252af66fab6875bcfc00c971e745512cdac
Angshuman16 <pathakangshu@gmail.com>
2023-09-24 01:27:39 +0530

    Bug Fixed1

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-const dispatch= useDispatch;
+
 
 
 const Header = () => {
