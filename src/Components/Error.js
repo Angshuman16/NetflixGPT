193d8f453042d7479ade63789ac3ae65d6d0e67b
Angshuman16 <pathakangshu@gmail.com>
2023-10-08 09:53:19 +0530

    Did some minor changes

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Error = () => {
   return (
+    <>
     <div className='text-red font-bold text-2xl'>An Error has Occured!</div>
+    <div>HEY ALL THIS IS FOR COMMITING</div>
+    </>
   )
 }
 
