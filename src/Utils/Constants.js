e25d826126f0f2b04f511fc3d24e269716b51f0a
Angshuman16 <pathakangshu@gmail.com>
2023-11-20 00:53:27 +0530

    Changed the key

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -8,5 +8,5 @@ export const API_OPTIONS = {
 
   export const IMG_CDN_URL="https://image.tmdb.org/t/p/w500";
 
-  export const OPENAI_KEY="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";
+  export const OPENAI_KEY="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";
 
